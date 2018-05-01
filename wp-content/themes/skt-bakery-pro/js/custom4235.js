7ac53045a539be9c84cb753604ec8f79ba2ceb0f
Pradeep Saini <pradeepsaini@Pradeeps-MacBook-Air.local>
2018-05-01 13:12:44 +0530

    hello

---
+++ wp-content/themes/skt-bakery-pro/js/custom4235.js
@@ -0,0 +1,188 @@
+jQuery(document).ready( function(){
+	var ww = jQuery(window).width();
+	
+	if( ww < 479){
+		jQuery('.news-box').removeClass('last');
+	}
+	if( ww > 479 && ww < 719){
+		jQuery('.client').removeClass('last');
+	}
+
+	// skill bar script
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+	
+	// team info display script
+	if(ww > 939){
+		jQuery('.team-info .info').hide();
+		jQuery('.team-info').mouseenter( function() {
+			jQuery(this).find('.info').slideDown();
+		}).mouseleave( function(){
+			jQuery(this).find('.info').slideUp();
+		});
+	}else{
+		jQuery('.team-info .info').show();
+	}
+
+	// accordion
+    jQuery('.accordion-box .acc-content').hide();
+    jQuery('.accordion-box h2:first').addClass('active').next().show();
+    jQuery('.accordion-box h2').click(function(){
+        if( jQuery(this).next().is(':hidden') ) {
+            jQuery('.accordion-box h2').removeClass('active').next().slideUp();
+            jQuery(this).toggleClass('active').next().slideDown();
+        }
+        return false; // Prevent the browser jump to the link anchor
+    });
+	
+	// Tabs
+	jQuery('ul.tabs > br').remove();
+	jQuery('.tabs-wrapper').append(jQuery('.tabs li div'));
+	jQuery('.tabs li:first a').addClass('defaulttab selected');
+	jQuery('.tabs a').click(function(){
+		switch_tabs(jQuery(this));
+	});
+	switch_tabs(jQuery('.defaulttab'));
+	function switch_tabs(obj) {
+		jQuery('.tab-content').hide();
+		jQuery('.tabs a').removeClass("selected");
+		var id = obj.attr("rel");
+		jQuery('#'+id).show();
+		obj.addClass("selected");
+	}
+
+	// Content Toggle
+    jQuery(".slide_toggle_content").hide();
+    jQuery("h3.slide_toggle").toggle(function(){
+	    jQuery(this).addClass("clicked");
+	}, function () {
+	    jQuery(this).removeClass("clicked");
+    });
+    jQuery("h3.slide_toggle").click(function(){
+		jQuery(this).next(".slide_toggle_content").slideToggle();
+    });
+
+});
+
+
+// NAVIGATION CALLBACK
+var ww = jQuery(window).width();
+jQuery(document).ready(function() { 
+	jQuery(".nav li a").each(function() {
+		if (jQuery(this).next().length > 0) {
+			jQuery(this).addClass("parent");
+		};
+	})
+	jQuery(".toggleMenu").click(function(e) { 
+		e.preventDefault();
+		jQuery(this).toggleClass("active");
+		jQuery(".nav").slideToggle('fast');
+	});
+	adjustMenu();
+})
+
+// navigation orientation resize callbak
+jQuery(window).bind('resize orientationchange', function() {
+	ww = jQuery(window).width();
+	adjustMenu();
+});
+
+var adjustMenu = function() {
+	if (ww < 980) {
+		jQuery(".toggleMenu").css("display", "block");
+		if (!jQuery(".toggleMenu").hasClass("active")) {
+			jQuery(".nav").hide();
+		} else {
+			jQuery(".nav").show();
+		}
+		jQuery(".nav li").unbind('mouseenter mouseleave');
+	} else {
+		jQuery(".toggleMenu").css("display", "none");
+		jQuery(".nav").show();
+		jQuery(".nav li").removeClass("hover");
+		jQuery(".nav li a").unbind('click');
+		jQuery(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
+			jQuery(this).toggleClass('hover');
+		});
+	}
+}
+
+jQuery(window).scroll(function() {
+		jQuery('.pleasure').each(function(){
+		var imagePos = jQuery(this).offset().top;
+
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeIn");
+			}
+		});
+		
+		jQuery('.our-products').each(function(){
+		var imagePos = jQuery(this).offset().top;
+
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInDown");
+			}
+		});		
+		
+		jQuery('.happycustomers').each(function(){
+		var imagePos = jQuery(this).offset().top;
+
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInLeft");
+			}
+		});	
+		
+		
+		jQuery('.ourbakers').each(function(){
+		var imagePos = jQuery(this).offset().top;
+
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInRight");
+			}
+		});	
+		
+		
+		jQuery('.whychoose').each(function(){
+		var imagePos = jQuery(this).offset().top;
+
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeIn");
+			}
+		});							
+		
+		jQuery('.contact-button').each(function(){
+		var imagePos = jQuery(this).offset().top;
+
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInUpBig");
+			}
+		});
+
+		jQuery('.bakeprocess').each(function(){
+		var imagePos = jQuery(this).offset().top;
+
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInUp");
+			}
+		});
+		
+		jQuery('.latestnews').each(function(){
+		var imagePos = jQuery(this).offset().top;
+
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInUp");
+			}
+		});
+		
+	});
